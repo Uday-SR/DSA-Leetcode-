55e28c75e0f1e67ccbbdd7d7b01fb15eedbf3531
Uday Singh Rana <udaysr2005@gmail.com>
2025-08-01 23:03:25 +0530

    Time: 0 ms (100%), Space: 9.6 MB (62.59%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -10,7 +10,7 @@ public:
             table[i][i] = 1;
         }
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 2; i < n; i++) {
             for(int j = 1; j < i; j++) {
                 table[i][j] = table[i-1][j]+table[i-1][j-1];
             }
